c40f8e92fc385e7a642a867d6a881c6ba4baf67f
SleakStick <benjamin@dropmann.ch>
2025-04-09 11:42:31 +0200

    Add latex compatibility

---
+++ lua/ben/plugins/latex.lua
@@ -0,0 +1,20 @@
+return {
+  "lervag/vimtex", -- Plugin repository
+  config = function()
+    -- Set the LaTeX compiler
+    vim.g.vimtex_compiler_method = "latexmk"
+
+    -- Configure PDF viewer (replace with your preferred viewer)
+    vim.g.vimtex_view_general_viewer = "zathura"
+    vim.g.vimtex_view_general_options = [[-reuse-instance -forward-search @tex @line @pdf]]
+    vim.g.vimtex_view_forward_search_on_start = 1 -- Auto-open PDF on compile
+
+    -- Enable conceal (optional, hides some LaTeX syntax for better readability)
+    vim.g.tex_conceal = "abdmg"
+
+    -- Keybindings (optional)
+    vim.keymap.set("n", "<leader>lc", ":VimtexCompile<CR>", { desc = "Compile LaTeX" })
+    vim.keymap.set("n", "<leader>lv", ":VimtexView<CR>", { desc = "View PDF" })
+    vim.keymap.set("n", "<leader>lx", ":VimtexStop<CR>", { desc = "Stop Compilation" })
+  end,
+}
