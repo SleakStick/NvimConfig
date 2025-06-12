87a4dc3b1059cfc706ea6625584a24b1762ac5b5
ben <benjamin@dropmann.ch>
2025-06-12 11:45:16 +0200

    Adjust viewer for latex files

--- lua/ben/plugins/latex.lua
+++ lua/ben/plugins/latex.lua
@@ -13,15 +13,12 @@ return {
 --    }
     vim.g.vimtex_compiler_method = "latexmk"
 
-    -- Configure PDF viewer (replace with your preferred viewer)
-    vim.g.vimtex_view_general_viewer = "zathura"
+    vim.g.vimtex_view_general_viewer = "org.gnome.Evince"
     vim.g.vimtex_view_general_options = [[-reuse-instance -forward-search @tex @line @pdf]]
     vim.g.vimtex_view_forward_search_on_start = 1 -- Auto-open PDF on compile
 
-    -- Enable conceal (optional, hides some LaTeX syntax for better readability)
     vim.g.tex_conceal = "abdmg"
 
-    -- Keybindings (optional)
     vim.keymap.set("n", "<leader>lc", ":VimtexCompile<CR>", { desc = "Compile LaTeX" })
     vim.keymap.set("n", "<leader>le", ":VimtexCompile<CR>", { desc = "Show vimtex errors"})
     vim.keymap.set("n", "<leader>lv", ":VimtexView<CR>", { desc = "View PDF" })
