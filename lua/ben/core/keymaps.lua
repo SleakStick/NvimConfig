2a7298283693b008598775bc48d04da7dfee0309
SleakStick <benjamin@dropmann.ch>
2025-10-31 19:21:45 +0100

    Add molten keymaps

--- lua/ben/core/keymaps.lua
+++ lua/ben/core/keymaps.lua
@@ -26,3 +26,30 @@ keymap.set("n", "<leader>ch", ":ToggleTerm direction=horizontal dir=./<CR>", {de
 keymap.set("n", "<leader>cf", ":ToggleTerm direction=float dir=./<CR>", {desc = "Toggle terminal as floating window"})
 keymap.set("n", "<leader>cv", ":ToggleTerm direction=vertical size=90 dir=./<CR>", {desc = "Toggle terminal as vertical spli"})
 keymap.set("n", "<leader>ct", ":ToggleTerm direction=tab dir=./<CR>", {desc = "Toggle terminal as new tab"})
+
+
+local function line_range_to_cell_create()   -- helper function for creation of cells in molten
+  local line_input = vim.fn.input("Extend range by N lines: ")
+  local line_amount = tonumber(line_input)
+
+  local total_lines = vim.api.nvim_buf_line_count(0)
+  local current_line, current_column = unpack(vim.api.nvim_win_get_cursor(0))
+  local end_line = line_amount + current_line 
+
+  if not line_amount or line_amount < 0 or end_line > total_lines then
+    vim.notify("Invalid or empty input. Command aborted.", vim.log.levels.WARN)
+    return
+  end
+
+  vim.fn.MoltenDefineCell(current_line, end_line)
+end
+
+
+keymap.set("n", "<leader>mi", ":MoltenInit<CR>" , { desc = "Initiate Molten" })
+keymap.set("n", "<leader>mn", line_range_to_cell_create, {desc = "Create Molten cell from current line to +N line"})
+keymap.set("n", "<leader>ml", ":MoltenEvaluateLine<CR>" , { desc = "Run line" })
+keymap.set("n", "<leader>mc", ":MoltenReevaluateCell<CR>" , { desc = "Run block" })
+keymap.set("n", "<leader>md", ":MoltenDelete<CR>" , { desc = "Run block" })
+vim.keymap.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv",{ desc = "Run visual selection" })
+
+
