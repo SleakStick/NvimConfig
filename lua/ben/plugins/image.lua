43c3159fc6df3f8b2e8e3df3d2c5fa858f490f97
SleakStick <benjamin@dropmann.ch>
2025-12-10 00:50:49 +0100

    Update commit2

--- lua/ben/plugins/image.lua
+++ lua/ben/plugins/image.lua
@@ -9,15 +9,15 @@ return {
   config=function()
     local image=require("image")
     -- if havnig issues, use
-    -- version = "1.1.0"
+    version = "1.1.0"
 
     image.setup({
       backend = "kitty", 
-      processor = "magick_cli", 
+      clear_in_insert_mode = true,
       integrations = {
         markdown = {
           enabled = true,
-          clear_in_insert_mode = false,
+          clear_in_insert_mode = true,
           download_remote_images = true,
           only_render_image_at_cursor = false,
           only_render_image_at_cursor_mode = "popup", 
@@ -44,10 +44,10 @@ return {
       max_width_window_percentage = math.huge,
       max_height_window_percentage = math.huge,
       scale_factor = 1.0,
-      window_overlap_clear_enabled = true, 
+      window_overlap_clear_enabled = false, 
       window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-      editor_only_render_when_focused = false, 
-      tmux_show_only_in_active_window = false, 
+      editor_only_render_when_focused = true, 
+      tmux_show_only_in_active_window = true, 
       hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, 
     })
   end
--- lua/ben/plugins/molten.lua
+++ lua/ben/plugins/molten.lua
@@ -10,6 +10,8 @@ return {
 
   config = function()
     vim.g.molten_image_provider = "image.nvim"
+    vim.g.molten_auto_image_popup = false
+    vim.g.molten_auto_open_output = false
     vim.g.molten_output_win_max_height = 20
   end,
 }
