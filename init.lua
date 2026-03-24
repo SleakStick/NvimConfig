7dc3829dddd26d07c27bd4f812b7b45d35237951
SleakStick <blueheaded@proton.me>
2026-03-24 16:17:22 +0100

    Add firenvim compatibility

--- init.lua
+++ init.lua
@@ -1,4 +1,22 @@
 require("ben.core")
 require("ben.lazy")
+-- this is firenvim specific
+if vim.g.started_by_firenvim == true then
+  vim.o.laststatus = 0
+  vim.o.showtabline = 0
+  vim.o.guifont = "monospace:h22" 
+end
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+    pattern = "texnique.xyz_*.txt",
+    command = "set filetype=tex"
+})
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+    pattern = "gemini.google.com_*.txt",
+    command = "set filetype=tex"
+})
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+    pattern = "overleaf.com_*.txt",
+    command = "set filetype=tex"
+})
 
 
--- lua/ben/plugins/colorscheme.lua
+++ lua/ben/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
+if vim.g.started_by_firenvim == true then
 return {
   {
     "folke/tokyonight.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
+      local bg = "#000000"
+      local bg_dark = "#000000"
+      local bg_highlight = "#4c4c4c"
       local bg_search = "#0A64AC"
       local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
+      local fg = "#e6e6e6"
+      local fg_dark = "#cccccc"
+      local fg_gutter = "#b2b2b2"
+      local border = "#000000"
       require("tokyonight").setup({
         style = "night",
         on_colors = function(colors)
@@ -38,3 +38,44 @@ return {
     end,
   },
 }
+else
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local bg = "#011628"
+      local bg_dark = "#011423"
+      local bg_highlight = "#143652"
+      local bg_search = "#0A64AC"
+      local bg_visual = "#275378"
+      local fg = "#CBE0F0"
+      local fg_dark = "#B4D0E9"
+      local fg_gutter = "#627E97"
+      local border = "#547998"
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          colors.bg = bg
+          colors.bg_dark = bg_dark
+          colors.bg_float = bg_dark
+          colors.bg_highlight = bg_highlight
+          colors.bg_popup = bg_dark
+          colors.bg_search = bg_search
+          colors.bg_sidebar = bg_dark
+          colors.bg_statusline = bg_dark
+          colors.bg_visual = bg_visual
+          colors.border = border
+          colors.fg = fg
+          colors.fg_dark = fg_dark
+          colors.fg_float = fg
+          colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
+        end,
+      })
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+}
+end
