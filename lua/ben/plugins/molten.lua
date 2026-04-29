f758c8cb2955a8ce32d1020f78daa608bdab44d7
SleakStick <blueheaded@proton.me>
2026-04-29 13:11:02 +0200

    Optimize slow plugins

--- lua/ben/plugins/molten.lua
+++ lua/ben/plugins/molten.lua
@@ -1,18 +1,18 @@
 return {
-  "benlubas/molten-nvim",
-
-  lazy = false,
-  version = "*",
-
-  build = function()
-    vim.cmd('UpdateRemotePlugins')
-  end,
-
-  config = function()
-    vim.g.molten_image_provider = "image.nvim"
-    vim.g.molten_auto_image_popup = false
-    vim.g.molten_auto_open_output = false
-    vim.g.molten_output_win_max_height = 20
-  end,
+--  "benlubas/molten-nvim",
+--
+--  lazy = false,
+--  version = "*",
+--
+--  build = function()
+--    vim.cmd('UpdateRemotePlugins')
+--  end,
+--
+--  config = function()
+--    vim.g.molten_image_provider = "image.nvim"
+--    vim.g.molten_auto_image_popup = false
+--    vim.g.molten_auto_open_output = false
+--    vim.g.molten_output_win_max_height = 20
+--  end,
 }
 
